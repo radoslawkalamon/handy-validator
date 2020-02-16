99f5f8856c6e09ad611765e805e726263c87cac4
Radoslaw Kalamon <kontakt+github@radoslawkalamon.pl>
2020-02-16 21:56:29 +0100

    [3.0.0] equalTo validator done

--- src/validators/equalTo.ts
+++ src/validators/equalTo.ts
@@ -1,1 +1,8 @@
-export default (): boolean => false;
+/**
+ * equalTo validator
+ * @version 1.0.0
+ * @param {any} value
+ * @param {any[]} arrayOfElements
+ * @returns {boolean}
+ */
+export default (value: any, arrayOfElements: any[]): boolean => arrayOfElements.indexOf(value) > -1;
