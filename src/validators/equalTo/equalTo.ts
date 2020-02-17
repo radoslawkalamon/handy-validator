0505da032a501eff99faecaaf7bea9044d4387be
Radoslaw Kalamon <kontakt+github@radoslawkalamon.pl>
2020-02-17 23:54:49 +0100

    [3.0.0] equalTo validator update

--- src/validators/equalTo/equalTo.ts
+++ src/validators/equalTo/equalTo.ts
@@ -1,8 +1,22 @@
+import errors from './equalTo.errors';
+
 /**
  * equalTo validator
- * @version 1.0.0
+ * @version 1.1.0
  * @param {any} value
  * @param {any[]} arrayOfElements
  * @returns {boolean}
  */
-export default (value: any, arrayOfElements: any[] = []): boolean => arrayOfElements.includes(value);
+export default (value: any, arrayOfElements: any[] = []): boolean => {
+  try {
+    if (!Array.isArray(arrayOfElements)) {
+      throw new Error(errors.arrayOfElementsNotAnArray);
+    }
+
+    return arrayOfElements.includes(value);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return false;
+  }
+};
--- tests/validators/equalTo/equalTo.spec.ts
+++ tests/validators/equalTo/equalTo.spec.ts
@@ -1,4 +1,5 @@
 import HandyValidator from '../../../src/index';
+import equalToErrors from '../../../src/validators/equalTo/equalTo.errors';
 
 let HandyVal: HandyValidator;
 
@@ -7,34 +8,64 @@ beforeAll(() => {
 });
 
 describe('equalTo validator tests', () => {
-  it('should return true if passed value is equal to one of validationArguments Array', () => {
-    const validator = 'equalTo';
-    const value = 'Good_value';
-    const validationArguments = ['Good_value', 'Bad_Value'];
+  describe('arrayOfElementsNotAnArray Error', () => {
+    let HandyValidatorResult: boolean;
+    let jestSpy: jest.SpyInstance;
 
-    expect(HandyVal.validate(validator, value, validationArguments)).toEqual(true);
+    beforeAll(() => {
+      jestSpy = jest.spyOn(console, 'error').mockImplementation(() => jest.fn());
+
+      const validator = 'equalTo';
+      const value = 'Value';
+      const validationArguments = 'Hemlo!';
+      // @ts-ignore
+      HandyValidatorResult = HandyVal.validate(validator, value, validationArguments);
+    });
+
+    it('should Handy Validator Result be false', () => {
+      expect(HandyValidatorResult).toEqual(false);
+    });
+
+    it('should call console.error once', () => {
+      expect(jestSpy.mock.calls.length).toBe(1);
+    });
+
+    it('should console.error message be validatorArrayGroupNotAnArray', () => {
+      const mockError = new Error(equalToErrors.arrayOfElementsNotAnArray);
+      expect(jestSpy.mock.calls[0][0]).toEqual(mockError);
+    });
+
+    afterAll(() => {
+      jestSpy.mockRestore();
+    });
   });
 
-  it('should return true if NaN is equal to one of validationArguments Array', () => {
-    const validator = 'equalTo';
-    const value = NaN;
-    const validationArguments = ['Good_value', NaN, 'Bad_Value'];
+  describe('Validator tests', () => {
+    it('should return true if passed value is equal to one of validationArguments Array', () => {
+      const validator = 'equalTo';
+      const value = 'Good_value';
+      const validationArguments = ['Good_value', 'Bad_Value'];
+      expect(HandyVal.validate(validator, value, validationArguments)).toEqual(true);
+    });
 
-    expect(HandyVal.validate(validator, value, validationArguments)).toEqual(true);
-  });
+    it('should return true if NaN is equal to one of validationArguments Array', () => {
+      const validator = 'equalTo';
+      const value = NaN;
+      const validationArguments = ['Good_value', NaN, 'Bad_Value'];
+      expect(HandyVal.validate(validator, value, validationArguments)).toEqual(true);
+    });
 
-  it('should return false if passed value is not equal to one of validationArguments Array', () => {
-    const validator = 'equalTo';
-    const value = 'Good_value';
-    const validationArguments = ['Bad_value_1', 'Bad_value_2', NaN];
+    it('should return false if passed value is not equal to one of validationArguments Array', () => {
+      const validator = 'equalTo';
+      const value = 'Good_value';
+      const validationArguments = ['Bad_value_1', 'Bad_value_2', NaN];
+      expect(HandyVal.validate(validator, value, validationArguments)).toEqual(false);
+    });
 
-    expect(HandyVal.validate(validator, value, validationArguments)).toEqual(false);
-  });
-
-  it('should return false if no validationArguments passed', () => {
-    const validator = 'equalTo';
-    const value = 'Good_value';
-
-    expect(HandyVal.validate(validator, value)).toEqual(false);
+    it('should return false if no validationArguments passed', () => {
+      const validator = 'equalTo';
+      const value = 'Good_value';
+      expect(HandyVal.validate(validator, value)).toEqual(false);
+    });
   });
 });
