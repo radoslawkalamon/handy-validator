6121bf8667270ce5f867e530abc0087bd64c2456
Radoslaw Kalamon <kontakt+github@radoslawkalamon.pl>
2020-02-20 22:58:53 +0100

    [3.0.0] Array validator done

---
+++ src/validators/array/array.validatorArrayGroup.typeGuard.ts
@@ -0,0 +1,35 @@
+import errors from './array.errors';
+import strategies from './array.strategies';
+
+const operatorsPermitted: string[] = Object.keys(strategies);
+/**
+ * @param {[string, number][]} validatorArrayGroup
+ * @returns validatorArray
+ */
+export default (validatorArrayGroup: [string, number][]): boolean => {
+  if (validatorArrayGroup && !Array.isArray(validatorArrayGroup)) {
+    throw new Error(errors.parentNotAnArray);
+  }
+
+  validatorArrayGroup.forEach((validatorArray) => {
+    if (!Array.isArray(validatorArray)) {
+      throw new Error(errors.itemNotAnArray);
+    }
+
+    if (validatorArray.length !== 2) {
+      throw new Error(errors.itemLengthError);
+    }
+
+    const [validatorOperator, validatorValue] = validatorArray;
+
+    if (typeof validatorOperator !== 'string' || typeof validatorValue !== 'number') {
+      throw new Error(errors.itemTypesError);
+    }
+
+    if (!operatorsPermitted.includes(validatorOperator)) {
+      throw new Error(errors.unknownOperator);
+    }
+  });
+
+  return true;
+};
