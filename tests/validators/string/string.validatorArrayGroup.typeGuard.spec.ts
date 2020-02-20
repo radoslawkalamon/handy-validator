6a3176bdd1d8d96eaf975e6eb13ae6fcad298b3f
Radoslaw Kalamon <kontakt+github@radoslawkalamon.pl>
2020-02-20 22:59:30 +0100

    [3.0.0] String validatorArrayGroupTypeGuard test

--- tests/validators/string/string.validatorArrayGroup.typeGuard.spec.ts
+++ tests/validators/string/string.validatorArrayGroup.typeGuard.spec.ts
@@ -12,27 +12,34 @@ describe('CheckValidatorArrayGroup tests', () => {
 
   it('should throw itemNotAnArray if validatorArray is not an Array', () => {
     expect(() => {
-      const validatorArrayGroup = [['<', 123], 'str'];
+      const validatorArrayGroup = [['=', 'string'], 'str'];
       validatorArrayGroupTypeGuard(validatorArrayGroup);
     }).toThrow(errors.itemNotAnArray);
   });
 
-  it('should throw itemLengthError if validatorArray length is not equail to validatorTypesArray', () => {
+  it('should throw itemLengthError if validatorArray length is not equal to 2', () => {
     expect(() => {
-      const validatorArrayGroup = [['>', 123, 123], ['<', 123]];
+      const validatorArrayGroup = [['!=', 123, 123], ['$', 123]];
       validatorArrayGroupTypeGuard(validatorArrayGroup);
     }).toThrow(errors.itemLengthError);
   });
 
-  it('should throw itemTypesError if validatorArray is not equal to validatorTypesArray types', () => {
+  it('should throw itemTypesError if validatorArray is not equal to prefered types', () => {
     expect(() => {
       const validatorArrayGroup = [[123, '<'], [undefined, {}]];
       validatorArrayGroupTypeGuard(validatorArrayGroup);
     }).toThrow(errors.itemTypesError);
   });
 
+  it('should throw itemTypesError if validatorArray is not equal to strategy types', () => {
+    expect(() => {
+      const validatorArrayGroup = [['!=', 'stringstring'], ['!=', 5]];
+      validatorArrayGroupTypeGuard(validatorArrayGroup);
+    }).toThrow(errors.itemTypesError);
+  });
+
   it('should return true if everything is okey', () => {
-    const validatorArrayGroup = [['jasd', 'asd'], ['asdasd', 'asdasd']];
+    const validatorArrayGroup = [['!=', 'stringstring'], ['len=', 5]];
     expect(validatorArrayGroupTypeGuard(validatorArrayGroup)).toEqual(true);
   });
 });
